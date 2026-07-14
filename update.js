6f6efa0529334af91d29cfbe2e46b173e8cce8a7
Roger Rodrigues <alewsor@gmail.com>
2026-07-14 14:45:23 +0530

    new arch

--- update.js
+++ update.js
@@ -1,7 +1,6 @@
 const getDataByStatus = require("./db/getPreviousData.js");
 const fetchPermitData = require("./permit_processors/permit.js");
-const cleanJSONinFolder = require("./utils/cleaner.js");
-const compareData = require("./utils/compare.js");
+const { comparePermitHashes } = require("./utils/hashes/hash.compare.js");
 const uploadFolder = require("./db/upload.js");
 const cleanupFolders = require("./utils/deleteFolders.js");
 
@@ -12,11 +11,10 @@ const status = "Inspection Phase";
 async function main() {
   const file = await getDataByStatus(status); //return filename
   await fetchPermitData(file);
-  await cleanJSONinFolder("permits", "cleaned_permits");
-  await compareData(file, "cleaned_permits", "DIFF_FOLDER");
+  await comparePermitHashes(file, "permits", "DIFF_FOLDER");
   await uploadFolder("DIFF_FOLDER");
-  // await cleanupFolders(["cleaned_permits", "DIFF_FOLDER", "permits"]);
-  // fs.rmSync(file, { force: true }); // remove inspection phase.json
+  await cleanupFolders(["DIFF_FOLDER", "permits"]);
+  fs.rmSync(file, { force: true }); // remove inspection phase.json
 }
 
 main();
